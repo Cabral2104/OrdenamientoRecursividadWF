8b96cc662a2f84e3c2859e40fbfffafb315abb53
Cabral2104 <valdezemilio50@gmail.com>
2023-08-31 18:29:44 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace OrdenamientoRecursividadWF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOrdenar = new System.Windows.Forms.Button();
+            this.listBoxArchivos = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnOrdenar
+            // 
+            this.btnOrdenar.Location = new System.Drawing.Point(657, 41);
+            this.btnOrdenar.Name = "btnOrdenar";
+            this.btnOrdenar.Size = new System.Drawing.Size(75, 23);
+            this.btnOrdenar.TabIndex = 0;
+            this.btnOrdenar.Text = "Ordenar";
+            this.btnOrdenar.UseVisualStyleBackColor = true;
+            this.btnOrdenar.Click += new System.EventHandler(this.btnOrdenar_Click);
+            // 
+            // listBoxArchivos
+            // 
+            this.listBoxArchivos.FormattingEnabled = true;
+            this.listBoxArchivos.Location = new System.Drawing.Point(106, 29);
+            this.listBoxArchivos.Name = "listBoxArchivos";
+            this.listBoxArchivos.Size = new System.Drawing.Size(509, 407);
+            this.listBoxArchivos.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBoxArchivos);
+            this.Controls.Add(this.btnOrdenar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOrdenar;
+        private System.Windows.Forms.ListBox listBoxArchivos;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OrdenamientoRecursividadWF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void OrdenarButton_Click(object sender, EventArgs e)
+        {
+           
+        }
+        private void ListarArchivosRecursivo(string directorio)
+        {
+            try
+            {
+                string[] archivos = Directory.GetFiles(directorio).OrderBy(fileName => fileName).ToArray();
+
+                foreach (string archivo in archivos)
+                {
+                    listBoxArchivos.Items.Add("Archivo: " + Path.GetFileName(archivo) + " - Extensión: " + Path.GetExtension(archivo));
+                }
+
+                string[] subdirectorios = Directory.GetDirectories(directorio);
+                foreach (string subdirectorio in subdirectorios)
+                {
+                    ListarArchivosRecursivo(subdirectorio);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnOrdenar_Click(object sender, EventArgs e)
+        {
+            using (var dialog = new FolderBrowserDialog())
+            {
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    string rutaCarpeta = dialog.SelectedPath;
+                    listBoxArchivos.Items.Clear(); // Limpiar lista antes de mostrar resultados
+                    ListarArchivosRecursivo(rutaCarpeta);
+                }
+            }
+        }
+    }
+}
+
